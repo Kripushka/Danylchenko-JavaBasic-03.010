686ea5378c3512ad972c788683f7d4f4ea088384
Kripushka <kristinadanylchenko@gmail.com>
2023-01-03 08:29:10 +0200

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float floatValue = 70.5f;
+        double doubleValue = 45.38;
+
+
+
+        System.out.println(doubleValue);
+    }
+}
